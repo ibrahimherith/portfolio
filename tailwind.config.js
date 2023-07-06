da947761b1c31ee530ea49df3a0f53e27f20dfa4
ibrahimherith <ibrahimherith19@gmail.com>
2023-07-06 16:06:54 +0300

    Updated files and folders

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,17 @@ module.exports = {
 
         extend: {
             colors: {
+                /* Primary */
+                DarkBlue: ("hsl(233, 26%, 24%)"),
+                LimeGreen: ("hsl(136, 65%, 51%)"),
+                BrightCyan: ("hsl(192, 70%, 51%)"),
+        
+                /* Neutral */
+                GrayishBlue: ("hsl(233, 8%, 62%)"),
+                LightGrayishBlue: ("hsl(220, 16%, 96%)"),
+                VeryLightGray: ("hsl(0, 0%, 98%)"),
+                White: ("hsl(0, 0%, 100%)"),
+
                 AlmostWhite: 'hsl(0, 0%, 98%)',
                 MediumGray: 'hsl(0, 0%, 41%)',
                 AlmostBlack: 'hsl(0, 0%, 8%)'
